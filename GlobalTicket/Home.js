c31e384855de1e1f6afa6d45c5be1324adf74c5d
msg4edson <msg.edson@gmail.com>
2022-02-13 11:37:33 -0300

    import StyleSheet and apply styles with StyleSheet.Create

--- GlobalTicket/Home.js
+++ GlobalTicket/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 
 const Home =(props) => {
   return (
-    <View style={{flexDirection: 'column'}}>
+    <View style={styles.container}>
       <Image
         style={style.globologo}
         source={require('./images/_Export_globoticket-bug-black.png')}
@@ -17,5 +17,31 @@ const Home =(props) => {
   );
 }
 const introText = `Praesentium aut quod et mollitia distinctio fuga. Est nobis ducimus tempore sunt ut minus. Assumenda harum dolores explicabo necessitatibus et. Voluptas ipsa voluptatibus dolor quia aperiam qui rerum. Quia sit corporis dolorum omnis inventore a. Sit velit earum mollitia animi.`
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: 20
+    },
+    textcontainer: {
+        textAlign: 'center',
+        paddingTop: 10
+    },
+    globologo: {
+        height: 100,
+        width: 150
+    },
+    title: {
+        fontFamily: 'Ubuntu-R'
+    },
+    subtitle: {
+        paddingTop: 5,
+        fontFamily: 'Ubuntu-R'
+    },
+    content: {
+        fontFamily: 'Ubuntu-L',
+        fontWeight: '300'
+    }
+});
 export default Home;
 
