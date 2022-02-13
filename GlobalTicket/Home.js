76a21b1ce6a828cf8d39e1f88fed45ff4fba2399
msg4edson <msg.edson@gmail.com>
2022-02-13 10:52:19 -0300

    Fixes #7

--- GlobalTicket/Home.js
+++ GlobalTicket/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View, Image } from 'react-native';
 
-const Home = props => {
-  return <View style={{flexDirection: 'column'}}></View>;
-};
-
+const Home =(props) => {
+  return (
+    <View style={{flexDirection: 'column'}}>
+      <Image
+        style={style.globologo}
+        source={require('./images/_Export_globoticket-bug-black.png')}
+      />
+    </View>
+  );
+}
 export default Home;
