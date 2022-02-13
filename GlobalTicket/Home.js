ff639ac079b2a6b1a6f1368fc69d705ef61f0e66
msg4edson <msg.edson@gmail.com>
2022-02-13 11:00:19 -0300

    Fixes #8

--- GlobalTicket/Home.js
+++ GlobalTicket/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text } from 'react-native';
 
 const Home =(props) => {
   return (
@@ -7,8 +7,15 @@ const Home =(props) => {
       <Image
         style={style.globologo}
         source={require('./images/_Export_globoticket-bug-black.png')}
-      />
+          />
+          <Text style={ styles.title }>Welcome TO GloboTicket</Text>
+          <Text style={styles.subtitle}>{ props.username}t</Text>
+          <View style={styles.textcontainer}>
+              <Text style={ styles.content }>{ introText }</Text>
+          </View>
     </View>
   );
 }
+const introText = `Praesentium aut quod et mollitia distinctio fuga. Est nobis ducimus tempore sunt ut minus. Assumenda harum dolores explicabo necessitatibus et. Voluptas ipsa voluptatibus dolor quia aperiam qui rerum. Quia sit corporis dolorum omnis inventore a. Sit velit earum mollitia animi.`
 export default Home;
+
