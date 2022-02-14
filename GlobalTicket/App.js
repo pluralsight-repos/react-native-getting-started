97126f4e89dd237390c7612c02866ff1e72fdc13
msg4edson <msg.edson@gmail.com>
2022-02-14 14:44:18 -0300

    Fixes #12

--- GlobalTicket/App.js
+++ GlobalTicket/App.js
@@ -10,13 +10,31 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import Home from './Home';
+
+const Stack = createStackNavigator();
 
 const App: () => Node = () => {
     return (    
         <>
             <StatusBar barStyle="dark-content" hidden />
-            <Home username='Sports Fan' />
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName='Home'
+                    headerMode='screen'
+                >
+                    <Stack.Screen
+                        name='Home'
+                        options={{
+                            headerShown: false
+                        }}
+                        >
+                            {(props) => <Home {...props}username='Sports Fan'/>}
+                        </Stack.Screen>
+                </Stack.Navigator>
+            </NavigationContainer>
         </>
     );
 };
