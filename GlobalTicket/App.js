901c2fd6fa22a38e603018f59c5f81e82a692519
msg4edson <msg.edson@gmail.com>
2022-02-14 14:21:56 -0300

    Fixes #11

--- GlobalTicket/App.js
+++ GlobalTicket/App.js
@@ -5,10 +5,11 @@
  * @format
  * @flow strict-local
  */
-
+ 
+import 'react-native-gesture-handler';
 import React from 'react';
-
 import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import Home from './Home';
 
 const App: () => Node = () => {
