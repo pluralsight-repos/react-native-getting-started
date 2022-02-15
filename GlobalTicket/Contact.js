977277a4a1abf39cd90f03dce3eb657364eb950e
msg4edson <msg.edson@gmail.com>
2022-02-15 11:58:05 -0300

    Fixes #18

--- GlobalTicket/Contact.js
+++ GlobalTicket/Contact.js
@@ -17,4 +17,48 @@ const Contact = ({navigation}) => {
         }
     }
 
+    return(
+        <View style={styles.form}>
+            <Text style={styles.label}>
+                Name: *required
+            </Text>
+            <TextInput
+                style={styles.txtinput}
+                onChangeText={name => setFormName(name)}
+                value={formName}
+                autoCapitalize='words'
+                selectTextOnFocus={true}
+            />
+            <Text style={styles.label}>
+                Email: *required
+            </Text>
+            <TextInput
+                style={style.txtinput}
+                onChangeText={email => setFormEmail(email)}
+                value={formEmail}
+                selectTextOnFocus={true}
+            />
+            <Text style={styles.label}>
+                Phone: 
+            </Text>
+            <TextInput
+                style={style.txtinput}
+                onChangeText={phone => setFormPhoneNumber(phone)}
+                value={formPhoneNumber}
+                selectTextOnFocus={true}
+            />
+            <Text style={styles.label}>
+                Message: *required 
+            </Text>
+            <TextInput
+                style={style.multtxtinput}
+                onChangeText={message => setFormMessage(message)}
+                value={formMessage}
+                multiline={true}
+                numberOfLines={3}
+                selectTextOnFocus={true}
+            />
+        </View>
+    )
+
 }
